022dba5a1f1fe9b996a79a77bbf302bd308b5d57
Ahmadabudaoud <ahmadabudaoud@gmail.com>
2021-05-17 16:00:33 +0300

    task

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = "ahmad";
+let age = 20;
+let hobby = "football";
+console.log(`Mr.${name}, in 33 years, you will be ${age + 33} years old . By the youll have decades of ${hobby} practice`)
+
